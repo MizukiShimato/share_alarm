edf5c192e960cfe1ac051c2178e1aec9b5dc4728
ryotamac <tennis.suta6@gmail.com>
2017-01-22 01:33:01 +0900

    アラーム取得先をメソッドに応じて変更

--- share_alarm/AlarmFireBaseGateway.swift
+++ share_alarm/AlarmFireBaseGateway.swift
@@ -9,30 +9,37 @@
 import Foundation
 import FirebaseDatabase
 
+private let userAlarmKey = "alarms"
+private let joinedUsersKey = "joinedUsers"
+
 class AlartFilebase {
-    let dbref: FIRDatabaseReference
+    let alarmDbref: FIRDatabaseReference
+    let userDbref: FIRDatabaseReference
     init() {
-        dbref = FIRDatabase.database().reference(withPath: "/alarm")
+        alarmDbref = FIRDatabase.database().reference(withPath: "/alarm")
+        userDbref = FIRDatabase.database().reference(withPath: "/user")
     }
+    
     func store(_ alarm: Alarm, userId: String) -> String {
-        let key = dbref.childByAutoId().key
-        dbref.child(userId).child(key).setValue(alarm.set(id: key).toHash())
+        let key = alarmDbref.childByAutoId().key
+        alarmDbref.child(userId).child(key).setValue(alarm.set(id: key).toHash())
+        userDbref.child(userId).child(userAlarmKey).child(key).setValue(alarm.set(id: key).toHash())
         return key
     }
     
     func updateUserStatus(userId: String, alarm: Alarm, user: JoinedUser) {
-        dbref.child(userId).child(alarm.id!).child("joinedUsers").child(user.id).setValue(user.toHash())
+        alarmDbref.child(userId).child(alarm.id!).child(joinedUsersKey).child(user.id).setValue(user.toHash())
     }
     
     func get(id: String, userId: String, cb: @escaping (Alarm?) -> Void) {
-        dbref.child(userId).child(id).observeSingleEvent(of: .value, with: { (db) in
+        userDbref.child(userId).child(userAlarmKey).child(id).observeSingleEvent(of: .value, with: { (db) in
             let alarm = AlartFilebase.toModel(db.value as! Dictionary<String, AnyObject>)
             cb(alarm)
         })
     }
     
     func get(userId: String, cb: @escaping ([Alarm]) -> Void) {
-        dbref.child(userId).observeSingleEvent(of: .value, with: { (db) in
+        userDbref.child(userId).child(userAlarmKey).observeSingleEvent(of: .value, with: { (db) in
             let dic = db.value as! Dictionary<String, AnyObject>
             cb(dic.flatMap({ model in
                 return AlartFilebase.toModel(model.value as! Dictionary<String, AnyObject>)
@@ -41,13 +48,13 @@ class AlartFilebase {
     }
     
     func subscribe(userId: String, alarm: Alarm, cb: @escaping ((Alarm?) -> Void)) -> UInt {
-        return dbref.child(userId).child(alarm.id!).observe(.value, with: { db in
+        return alarmDbref.child(userId).child(alarm.id!).observe(.value, with: { db in
             cb(AlartFilebase.toModel(db.value as! Dictionary<String, AnyObject>))
         })
     }
     
     func unsubscribe(userId: String, alarm: Alarm, handlerId: UInt) {
-        dbref.child(userId).child(alarm.id!).removeObserver(withHandle: handlerId)
+        alarmDbref.child(userId).child(alarm.id!).removeObserver(withHandle: handlerId)
     }
     
     static func joinedUserToModel(_ dict: [String: AnyObject]) -> JoinedUser? {
@@ -82,7 +89,7 @@ class AlartFilebase {
             
             var joinedUsers: [JoinedUser] = []
             
-            if let users = dict["joinedUsers"] {
+            if let users = dict[joinedUsersKey] {
                 joinedUsers = (users as! Dictionary<String, AnyObject?>).flatMap { user in
                     return AlartFilebase.joinedUserToModel(user.value as! Dictionary<String, AnyObject>)
                 }
